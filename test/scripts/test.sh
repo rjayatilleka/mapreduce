8b3611f3582dd11a133327a97ca8ee5509690906
Ramith Jayatilleka <ramith.jayatilleka@gmail.com>
2016-04-20 22:13:04 -0500

    to 10000000

--- test/scripts/test.sh
+++ test/scripts/test.sh
@@ -3,9 +3,9 @@
 rm -rf "$HOME/logs"
 mkdir "$HOME/logs"
 
-chunkSize=100000
+chunkSize=1000000
 cpm=8
-inputFile=1000000
+inputFile=10000000
 
 for redundancy in {1..4}; do
   for failProb in {0..90..10}; do
